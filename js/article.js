a64c4c7d5042aabb78480af9d23a74dd7d1a37be
SufCode <1841830683@qq.com>
2018-07-28 22:30:39 +0800

    7月28号

--- js/article.js
+++ js/article.js
@@ -3,143 +3,143 @@ var alertTitle = $('<span class="alertSpan"></span>');
 var onOff = true;
 var Judge = true;
 var titleImg = $('#ContainEditor img')
-    .eq(0)
-    .attr('src');
+  .eq(0)
+  .attr('src');
 $('#ContainEditor h1')
-    .eq(0)
-    .css({
-        display: 'none',
-    });
+  .eq(0)
+  .css({
+    display: 'none',
+  });
 var getNodeName = document.getElementById('ContainEditor').firstElementChild
-    .nodeName;
+  .nodeName;
 var addContent = $('<' + getNodeName + '></' + getNodeName + '>');
 addContent.insertBefore($('#userInformation'));
 var getFirstContent = document.getElementById('ContainEditor').firstElementChild
-    .innerHTML;
+  .innerHTML;
 addContent.html(getFirstContent);
 
 var ListLan = $('.circle').attr('list-title');
 
 $('.timeBar').hover(
-    function () {
-        $('.bottom_message').append(alertTitle);
-        $('.alertSpan').html(DataTitle);
-        setTimeout(function () {
-            $('.alertSpan').css({
-                opacity: '1',
-            });
-        }, 10);
-    },
-    function () {
-        $('.alertSpan').css({
-            opacity: '',
-        });
-        setTimeout(function () {
-            alertTitle.remove();
-        }, 300);
-    }
-)
-
-$('.circle').hover(
-    function () {
-        $('.ContentShare').append(alertTitle);
-        $('.alertSpan').html(ListLan);
-        setTimeout(function () {
-            $('.alertSpan').css({
-                opacity: '1',
-            });
-        }, 10);
-    },
-    function () {
-        $('.alertSpan').css({
-            opacity: '',
-        });
-        setTimeout(function () {
-            alertTitle.remove();
-        }, 300);
-    }
+  function() {
+    $('.bottom_message').append(alertTitle);
+    $('.alertSpan').html(DataTitle);
+    setTimeout(function() {
+      $('.alertSpan').css({
+        opacity: '1',
+      });
+    }, 10);
+  },
+  function() {
+    $('.alertSpan').css({
+      opacity: '',
+    });
+    setTimeout(function() {
+      alertTitle.remove();
+    }, 300);
+  },
 );
 
-$('.focus').on('click', function () {
-    if (onOff) {
-        onOff = false;
-        $('.focus').css({
-            background: '#869B74',
-        });
-        $('.focus .iconfont').html('&#xe642;');
-        $('.focus span').html('已关注');
-    } else {
-        onOff = true;
-        $('.focus').css({
-            background: '',
-        });
-        $('.focus .iconfont').html('&#xe604;');
-        $('.focus span').html('关注');
-    }
+$('.circle').hover(
+  function() {
+    $('.ContentShare').append(alertTitle);
+    $('.alertSpan').html(ListLan);
+    setTimeout(function() {
+      $('.alertSpan').css({
+        opacity: '1',
+      });
+    }, 10);
+  },
+  function() {
+    $('.alertSpan').css({
+      opacity: '',
+    });
+    setTimeout(function() {
+      alertTitle.remove();
+    }, 300);
+  },
+);
+
+$('.focus').on('click', function() {
+  if (onOff) {
+    onOff = false;
+    $('.focus').css({
+      background: '#869B74',
+    });
+    $('.focus .iconfont').html('&#xe642;');
+    $('.focus span').html('已关注');
+  } else {
+    onOff = true;
+    $('.focus').css({
+      background: '',
+    });
+    $('.focus .iconfont').html('&#xe604;');
+    $('.focus span').html('关注');
+  }
 });
 
 var UrlAddress = 'https://music.163.com';
 
 //配置share.js的参数
 socialShare('.social-share', {
-    title: getFirstContent, //分享的标题
-    image: titleImg, //分享的图片  一般是正文的第一张图片
-    url: UrlAddress, //填写当前页面的地址     window.location.href      分享的地址
-    description: getFirstContent, //分享到额描述
+  title: getFirstContent, //分享的标题
+  image: titleImg, //分享的图片  一般是正文的第一张图片
+  url: UrlAddress, //填写当前页面的地址     window.location.href      分享的地址
+  description: getFirstContent, //分享到额描述
 });
 
-$('.circle').on('click', function () {
-    if (Judge) {
-        Judge = false;
-        $('.listTop')
-            .eq(0)
-            .css({
-                top: '50%',
-                transform: 'translate(-50%,-50%) rotateZ(-45deg)',
-            });
-        $('.listTop')
-            .eq(2)
-            .css({
-                top: '50%',
-                transform: 'translate(-50%,-50%) rotateZ(45deg)',
-            });
-        $('.listTop')
-            .eq(1)
-            .css({
-                display: 'none',
-            });
-        $('.social-share').css({
-            display: 'block',
-            height: '600px',
-        });
-    } else {
-        Judge = true;
-        $('.listTop')
-            .eq(0)
-            .css({
-                top: '',
-                transform: '',
-            });
-        $('.listTop')
-            .eq(2)
-            .css({
-                top: '',
-                transform: '',
-            });
-        $('.listTop')
-            .eq(1)
-            .css({
-                display: '',
-            });
-        $('.social-share').css({
-            height: '',
-        });
-        setTimeout(function () {
-            $('.social-share').css({
-                display: '',
-            });
-        }, 200);
-    }
+$('.circle').on('click', function() {
+  if (Judge) {
+    Judge = false;
+    $('.listTop')
+      .eq(0)
+      .css({
+        top: '50%',
+        transform: 'translate(-50%,-50%) rotateZ(-45deg)',
+      });
+    $('.listTop')
+      .eq(2)
+      .css({
+        top: '50%',
+        transform: 'translate(-50%,-50%) rotateZ(45deg)',
+      });
+    $('.listTop')
+      .eq(1)
+      .css({
+        display: 'none',
+      });
+    $('.social-share').css({
+      display: 'block',
+      height: '600px',
+    });
+  } else {
+    Judge = true;
+    $('.listTop')
+      .eq(0)
+      .css({
+        top: '',
+        transform: '',
+      });
+    $('.listTop')
+      .eq(2)
+      .css({
+        top: '',
+        transform: '',
+      });
+    $('.listTop')
+      .eq(1)
+      .css({
+        display: '',
+      });
+    $('.social-share').css({
+      height: '',
+    });
+    setTimeout(function() {
+      $('.social-share').css({
+        display: '',
+      });
+    }, 200);
+  }
 });
 
 //分享菜单可移动
@@ -150,52 +150,52 @@ var C_l = 0;
 var C_t = 0;
 var isDown = false;
 //鼠标按下事件
-ContentShare.onmousedown = function (e) {
-    //获取x坐标和y坐标
-    C_x = e.clientX;
-    C_y = e.clientY;
+ContentShare.onmousedown = function(e) {
+  //获取x坐标和y坐标
+  C_x = e.clientX;
+  C_y = e.clientY;
 
-    //获取左部和顶部的偏移量
-    C_l = ContentShare.offsetLeft;
-    C_t = ContentShare.offsetTop;
-    //开关打开
-    isDown = true;
+  //获取左部和顶部的偏移量
+  C_l = ContentShare.offsetLeft;
+  C_t = ContentShare.offsetTop;
+  //开关打开
+  isDown = true;
 };
 //鼠标移动
-window.onmousemove = function (e) {
-    if (isDown == false) {
-        return;
-    }
-    //获取x和y
-    var nx = e.clientX;
-    var ny = e.clientY;
-    //计算移动后的左偏移量和顶部的偏移量
-    var nl = nx - (C_x - C_l);
-    var nt = ny - (C_y - C_t);
+window.onmousemove = function(e) {
+  if (isDown == false) {
+    return;
+  }
+  //获取x和y
+  var nx = e.clientX;
+  var ny = e.clientY;
+  //计算移动后的左偏移量和顶部的偏移量
+  var nl = nx - (C_x - C_l);
+  var nt = ny - (C_y - C_t);
 
-    if (nt >= 150) {
-        ContentShare.style.left = nl + 'px';
-        ContentShare.style.top = nt + 'px';
-    }
+  if (nt >= 150) {
+    ContentShare.style.left = nl + 'px';
+    ContentShare.style.top = nt + 'px';
+  }
 
-    $('body').css({
-        '-moz-user-select': 'none',
-        '-webkit-user-select': 'none',
-        '-ms-user-select': 'none',
-        'user-select': 'none',
-    });
+  $('body').css({
+    '-moz-user-select': 'none',
+    '-webkit-user-select': 'none',
+    '-ms-user-select': 'none',
+    'user-select': 'none',
+  });
 };
 //鼠标抬起事件
-ContentShare.onmouseup = function () {
-    //开关关闭
-    isDown = false;
+ContentShare.onmouseup = function() {
+  //开关关闭
+  isDown = false;
 
-    $('body').css({
-        '-moz-user-select': '',
-        '-webkit-user-select': '',
-        '-ms-user-select': '',
-        'user-select': '',
-    });
+  $('body').css({
+    '-moz-user-select': '',
+    '-webkit-user-select': '',
+    '-ms-user-select': '',
+    'user-select': '',
+  });
 };
 
 //创建一个新的编辑器
@@ -208,164 +208,193 @@ editor.create();
 $('.publish_A').html('<i class="iconfont">&#xe815;</i>发送');
 
 $('#QRCode').qrcode({
-    width: 105,
-    height: 105,
-    text: UrlAddress,
+  width: 105,
+  height: 105,
+  text: UrlAddress,
 });
 
 var initNum = 100;
 $('.numCount').html(initNum);
 var isGood = true;
 var isBad = true;
-$('.TwoPart a').eq(0).on('click', function () {
+$('.TwoPart a')
+  .eq(0)
+  .on('click', function() {
     if (isGood) {
-        isGood = false;
-        Change(1, $(this), 1);
+      isGood = false;
+      Change(1, $(this), 1);
     } else {
-        isGood = true;
-        Change(0, $(this), 1);
+      isGood = true;
+      Change(0, $(this), 1);
     }
-});
+  });
 
-$('.TwoPart a').eq(1).on('click', function () {
+$('.TwoPart a')
+  .eq(1)
+  .on('click', function() {
     if (isBad) {
-        isBad = false;
-        Change(1, $(this), 0);
+      isBad = false;
+      Change(1, $(this), 0);
     } else {
-        isBad = true;
-        Change(0, $(this), 0);
+      isBad = true;
+      Change(0, $(this), 0);
     }
-});
+  });
 
 function Change(n, m, o) {
-    if (n) {
-        if (o) {
-            initNum++;
-        }
-        m.css({
-            'color': '#FF9500',
-        });
-    } else {
-        if (o) {
-            initNum--;
-        }
-        m.css({
-            'color': '',
-        });
+  if (n) {
+    if (o) {
+      initNum++;
     }
-    $('.numCount').html(initNum);
+    m.css({
+      color: '#FF9500',
+    });
+  } else {
+    if (o) {
+      initNum--;
+    }
+    m.css({
+      color: '',
+    });
+  }
+  $('.numCount').html(initNum);
 }
 
 var NumAdd = 0;
 
 // description.replace(/<(?!img).*?>/g, "");//去除所有的html标签除了img
-$('.NewEditor .publish_A').on('click', function () {
-    NumAdd++;
-    var Now = new Date();
-    var Year = Now.getFullYear();
-    var Month = Now.getMonth() + 1;
-    var Day = Now.getDate();
-    var Hour = Now.getHours();
-    var Minute = Now.getMinutes();
-    var Second = Now.getSeconds();
-    var addComments = $('<li class="Number' + NumAdd + '"><div class="commentsMessage"><div class="topMessage"><a href="javascript:;"><img src="../img/11.jpg"></a>' +
-        '<div class="rightMessage"><div class="commentsName">白矖</div><div class="timeMessage"><span></span><span></span>' +
-        '</div></div></div><div class="bottomMessage"><p></p><div class="toolBar_Btn"><a href="javascript:;"><i class="iconfont">&#xe606;</i>' +
-        '<span class="goodNum">0</span><span>人赞</span></a><a href="javascript:;"><i class="iconfont">&#xe61b;</i><span>回复</span>' +
-        '</a></div> </div></div></li>');
-    var Content = $('.NewEditor .w-e-text').html().replace(/<(?!img).*?>/g, "");
-    $('.commentsList').prepend(addComments);
-    $('.Number' + NumAdd + ' .commentsMessage .bottomMessage p').html(Content);
-    $('.Number' + NumAdd + ' .timeMessage span').eq(0).html('' + Year + '/' + Month + '/' + Day + '');
-    $('.Number' + NumAdd + ' .timeMessage span').eq(1).html('' + addZero(Hour) + ':' + addZero(Minute) + ':' + addZero(Second) + '');
-    $('.NewEditor .w-e-text').html('<p><br></p>');
+$('.NewEditor .publish_A').on('click', function() {
+  NumAdd++;
+  var Now = new Date();
+  var Year = Now.getFullYear();
+  var Month = Now.getMonth() + 1;
+  var Day = Now.getDate();
+  var Hour = Now.getHours();
+  var Minute = Now.getMinutes();
+  var Second = Now.getSeconds();
+  var addComments = $(
+    '<li class="Number' +
+      NumAdd +
+      '"><div class="commentsMessage"><div class="topMessage"><a href="javascript:;"><img src="../img/11.jpg"></a>' +
+      '<div class="rightMessage"><div class="commentsName">白矖</div><div class="timeMessage"><span></span><span></span>' +
+      '</div></div></div><div class="bottomMessage"><p></p><div class="toolBar_Btn"><a href="javascript:;"><i class="iconfont">&#xe606;</i>' +
+      '<span class="goodNum">0</span><span>人赞</span></a><a href="javascript:;"><i class="iconfont replyBack">&#xe61b;</i><span>回复</span>' +
+      '</a></div> </div></div></li>',
+  );
+  var Content = $('.NewEditor .w-e-text')
+    .html()
+    .replace(/<(?!img).*?>/g, '');
+  $('.commentsList').prepend(addComments);
+  $('.Number' + NumAdd + ' .commentsMessage .bottomMessage p').html(Content);
+  $('.Number' + NumAdd + ' .timeMessage span')
+    .eq(0)
+    .html('' + Year + '/' + Month + '/' + Day + '');
+  $('.Number' + NumAdd + ' .timeMessage span')
+    .eq(1)
+    .html(
+      '' + addZero(Hour) + ':' + addZero(Minute) + ':' + addZero(Second) + '',
+    );
+  $('.NewEditor .w-e-text').html('<p><br></p>');
 
-    //点赞
-    var NotSame = true;
-    var GoodNum = parseInt($('.goodNum').html());
-    $('.toolBar_Btn a').eq(0).on('click', function () {
-        if (NotSame) {
-            NotSame = false;
-            onNotSame($(this).find('.iconfont'), 1);
-            $(this).find('.goodNum').html(++GoodNum);
-        } else {
-            NotSame = true;
-            onNotSame($(this).find('.iconfont'), 0);
-            $(this).find('.goodNum').html(--GoodNum);
-        }
+  //点赞
+  var NotSame = true;
+  var GoodNum = parseInt($('.goodNum').html());
+  $('.toolBar_Btn a')
+    .eq(0)
+    .on('click', function() {
+      if (NotSame) {
+        NotSame = false;
+        onNotSame($(this).find('.iconfont'), 1);
+        $(this)
+          .find('.goodNum')
+          .html(++GoodNum);
+      } else {
+        NotSame = true;
+        onNotSame($(this).find('.iconfont'), 0);
+        $(this)
+          .find('.goodNum')
+          .html(--GoodNum);
+      }
     });
 
-    //回复
-    var NewGoodEditor = $('<div class="NewGoodEditor"><div class="NewEditor">' +
-        '<div id="Newtoolbar" class="NewToolbar" style="width:100%;background: #fff;border-bottom: 1px solid #DDD;"></div>' +
-        '<div id="NewUser_edit" class="EditorNew" style="width:100%;height:200px;display: flex;justify-content: center;' +
-        'align-content: center;flex-wrap:wrap;background:#fff;"></div></div></div>');
-    var NoSame = true;
-    $('.toolBar_Btn a').eq(1).on('click', function () {
-        if (NoSame) {
-            NoSame = false;
-            onNotSame($(this).find('.iconfont'), 1);
-            $(this).parent().parent().append(NewGoodEditor);
-            setTimeout(function () {
-                $('.NewGoodEditor').css({
-                    'opacity': '1',
-                    'top': '0',
-                });
-                $('.NewGoodEditor .cancel_A').css({
-                    'display': 'flex',
-                });
-                $('.NewGoodEditor .publish_A').html('<i class="iconfont">&#xe815;</i>发送');
-            }, 10);
-            var M = window.wangEditor;
-            var NewEditor = new M('#Newtoolbar', '#NewUser_edit');
-            NewEditor.customConfig.menus = ['bold', 'italic', 'link', 'emoticon'];
-            NewEditor.customConfig.zIndex = 0;
-            NewEditor.create();
+  //回复
+  var NewGoodEditor = $(
+    '<div class="NewGoodEditor"><div class="NewEditor">' +
+      '<div id="Newtoolbar" class="NewToolbar" style="width:100%;background: #fff;border-bottom: 1px solid #DDD;"></div>' +
+      '<div id="NewUser_edit" class="EditorNew" style="width:100%;height:200px;display: flex;justify-content: center;' +
+      'align-content: center;flex-wrap:wrap;background:#fff;"></div></div></div>',
+  );
 
-            $('.NewGoodEditor .cancel_A').on('click', function () {
-                CodeSame(NewGoodEditor);
-                $('.toolBar_Btn a').eq(1).find('.iconfont').css({
-                    'color': '',
-                });
-                NoSame = true;
-            });
-        } else {
-            NoSame = true;
-            onNotSame($(this).find('.iconfont'), 0);
-            CodeSame(NewGoodEditor);
-        }
+  $('.toolBar_Btn a')
+    .eq(1)
+    .on('click', function() {
+      $('.replyBack').css({
+        color: '',
+      });
+      CodeSame($('.NewGoodEditor'));
+      onNotSame($(this).find('.iconfont'), 1);
+      $(this)
+        .parent()
+        .parent()
+        .append(NewGoodEditor);
+      setTimeout(function() {
+        $('.NewGoodEditor').css({
+          opacity: '1',
+          top: '0',
+        });
+        $('.NewGoodEditor .cancel_A').css({
+          display: 'flex',
+        });
+        $('.NewGoodEditor .publish_A').html(
+          '<i class="iconfont">&#xe815;</i>发送',
+        );
+      }, 10);
+      var M = window.wangEditor;
+      var NewEditor = new M('#Newtoolbar', '#NewUser_edit');
+      NewEditor.customConfig.menus = ['bold', 'italic', 'link', 'emoticon'];
+      NewEditor.customConfig.zIndex = 0;
+      NewEditor.create();
+
+      $('.NewGoodEditor .cancel_A').on('click', function() {
+        CodeSame(NewGoodEditor);
+        $('.replyBack').css({
+          color: '',
+        });
+      });
     });
 });
 
 function addZero(n) {
-    if (n < 10) {
-        n = '0' + n + ''
-    }
-    return n;
+  if (n < 10) {
+    n = '0' + n + '';
+  }
+  return n;
 }
 
 function onNotSame(m, n) {
-    if (n) {
-        m.css({
-            'color': '#00B38C',
-        });
-    } else {
-        m.css({
-            'color': '',
-        });
-    };
-};
-
-function CodeSame(n) {
-    $('.NewGoodEditor').css({
-        'opacity': '',
-        'top': '',
+  if (n) {
+    m.css({
+      color: '#00B38C',
     });
-    $('.NewGoodEditor .w-e-text').remove();
-    n.remove();
+  } else {
+    m.css({
+      color: '',
+    });
+  }
 }
 
-$(document).on("click", '.toolBar_Btn a,.NewEditor .publish_A', function () {
-    $("body").getNiceScroll().resize();
+function CodeSame(n) {
+  $('.NewGoodEditor').css({
+    opacity: '',
+    top: '',
+  });
+  $('.NewGoodEditor .w-e-text').remove();
+  n.remove();
+}
+
+$(document).on('click', '.toolBar_Btn a,.NewEditor .publish_A', function() {
+  $('body')
+    .getNiceScroll()
+    .resize();
 });
 //重载滚动条
